a2940c53a408438e3d79a788615a633a2d8c36ab
Tyler Croley <tjcroley@outlook.com>
2020-02-23 21:54:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,7 +22,7 @@ end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_supplies[:winter][:christmas] = "Balloons"
-  
+  holiday_supplies[:winter][:new_years] = "Balloons"
 
 end
 
